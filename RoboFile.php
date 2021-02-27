74d24c6462f8831aa3ecbb70121ea1ef3d1869f2
EasyChen <easychen@qq.com>
2021-02-27 23:10:49 +0800

    update

---
+++ RoboFile.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    // define public methods as commands
+    public function up( $note = 'update' )
+    {
+        $repo = "https://gitlab.com/easychen/memberprism2.git";
+        $this->_exec("git add . && git commit -m '$note' && git push");
+        $tmp_dir = '/tmp/MPT'.md5($repo);
+        if( file_exists( $tmp_dir ) )
+        {
+            $this->_exec("cd $tmp_dir && git pull");
+        }
+        else
+        {
+            $this->_exec("git clone $repo $tmp_dir");
+        }
+        $this->_exec("cd $tmp_dir && rm -rf .git && cp -rf ./* /Users/Easy/Code/gitcode/MemberPrism2/ && cp -rf ./.vscode /Users/Easy/Code/gitcode/MemberPrism2/ && cp -rf ./.vscode /Users/Easy/Code/gitcode/MemberPrism2/ && git add . && git commit -m '$note' && git push");
+
+    }
+}
